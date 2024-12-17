4e92ad907226116130f4f885b8b0b91d183a894c
jaizu23 <157589155+jaizu23@users.noreply.github.com>
2024-12-17 19:44:33 +0100

    Hasta ej7 sin revisar

--- PECL3.sql
+++ PECL3.sql
@@ -1,7 +1,7 @@
 
---consultas yb arreglos de la base de datos de la PECL2
+-- Consultas y arreglos de la base de datos de la PECL2
 
---Ejercicio1:
+-- Ejercicio1:
 
 DELETE FROM final.vehículos WHERE vehicle_id is NULL or vehicle_id LIKE '' or length(vehicle_id) < 10;
 
@@ -9,14 +9,22 @@ DELETE FROM final.colision_vehiculo WHERE vehicle_id is NULL or vehicle_id LIKE 
 
 ALTER TABLE final.colision_persona DROP COLUMN vehicle_id;
 
---Ejercicio2:
+-- Ejercicio2:
 
-DELETE FROM final.personas WHERE personas.person_id is NULL or personas.person_id LIKE '' or length(person_id) < 10;
+DELETE FROM final.personas
+WHERE personas.person_id is NULL or personas.person_id LIKE '' or length(person_id) < 10;
 
---Ejercicio3:
+-- Ejercicio3:
 
+ALTER TABLE final.vehículos
+ADD COLUMN state_registration varchar(2);
 
---Ejercicio4:
+UPDATE final.vehículos as v
+SET state_registration = cv.state_registration
+FROM final.colision_vehiculo as cv
+WHERE v.vehicle_id = cv.vehicle_id;
+
+-- Ejercicio4:
 
 UPDATE final.vehículos
 SET vehicle_type = 'unknown'
@@ -36,4 +44,69 @@ WHERE vehicle_year is NULL or vehicle_year LIKE '';
 
 UPDATE final.vehículos
 SET state_registration = 'unknown'                              --falta crear una columna state_registration con todos los valores a nulo.
-WHERE  state_registration NULL or state_registration LIKE '';
+WHERE state_registration NULL or state_registration LIKE '';
+
+-- Ejercicio 5:
+
+ALTER TABLE final.personas
+ADD COLUMN person_sex char(1);
+
+UPDATE final.personas
+SET person_sex = final.colision_persona.person_sex
+WHERE person_id = final.colision_persona.person_id;
+
+ALTER TABLE final.colision_persona
+DROP COLUMN person_sex;
+
+UPDATE final.personas
+SET person_sex = 'U'
+WHERE person_sex NULL or person_sex LIKE '';
+
+-- Ejercicio 6:
+
+ALTER TABLE final.personas
+ADD COLUMN person_age INT;
+
+CREATE OR REPLACE FUNCTION calcular_edad()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.person_dob IS NOT NULL THEN
+        NEW.person_age := DATE_PART('year', AGE(NEW.person_dob));
+    ELSE
+        NEW.person_age := NULL;
+    END IF;
+    RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER calcular_edad
+BEFORE INSERT ON final.personas
+FOR EACH ROW
+EXECUTE FUNCTION calcular_edad();
+
+-- Ejercicio 7
+
+ALTER TABLE final.vehículos
+ADD COLUMN vehicle_accidents INT;
+
+ALTER TABLE final.personas
+ADD COLUMN person_age INT;
+
+CREATE OR REPLACE FUNCTION calcular_accidentes()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.person_dob IS NOT NULL THEN
+        NEW.person_age := DATE_PART('year', AGE(NEW.person_dob));
+    ELSE
+        NEW.person_age := NULL;
+    END IF;
+    RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER calcular_accidentes
+BEFORE INSERT ON final.personas
+FOR EACH ROW
+EXECUTE FUNCTION calcular_accidentes();
